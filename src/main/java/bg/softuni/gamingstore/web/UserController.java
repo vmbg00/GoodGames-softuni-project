a8bfca9c2397d6bf5ba4f58d5860ca94184896ae
vmbg00 <vmbg00@gmail.com>
2021-03-04 01:54:15 +0200

    Created a register page.

---
+++ src/main/java/bg/softuni/gamingstore/web/UserController.java
@@ -0,0 +1,15 @@
+package bg.softuni.gamingstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
