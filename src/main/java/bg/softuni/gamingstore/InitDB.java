a01de03ee47227487cbe47bc14d2a32e00ce0c3e
vmbg00 <vmbg00@gmail.com>
2021-03-05 01:28:03 +0200

    Adds new user with admin role if the UserRepository is empty

--- src/main/java/bg/softuni/gamingstore/InitDB.java
+++ src/main/java/bg/softuni/gamingstore/InitDB.java
@@ -1,10 +1,13 @@
 package bg.softuni.gamingstore;
 
 import bg.softuni.gamingstore.models.entities.RoleEntity;
+import bg.softuni.gamingstore.models.entities.UserEntity;
 import bg.softuni.gamingstore.models.entities.enums.RoleEnums;
 import bg.softuni.gamingstore.repositories.RolesRepository;
+import bg.softuni.gamingstore.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,10 +16,14 @@ import java.util.List;
 public class InitDB implements CommandLineRunner {
 
     private final RolesRepository rolesRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     @Autowired
-    public InitDB(RolesRepository rolesRepository) {
+    public InitDB(RolesRepository rolesRepository, PasswordEncoder passwordEncoder, UserRepository userRepository) {
         this.rolesRepository = rolesRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -31,6 +38,22 @@ public class InitDB implements CommandLineRunner {
             user.setDescription("Peasant");
 
             this.rolesRepository.saveAll(List.of(admin, user));
+        }
+
+        if (userRepository.count() == 0){
+            UserEntity userEntity = new UserEntity();
+
+            RoleEntity admin = this.rolesRepository.findByName(RoleEnums.ADMIN);
+            RoleEntity user = this.rolesRepository.findByName(RoleEnums.USER);
+
+            userEntity
+                    .setUsername("admin")
+                    .setPassword(this.passwordEncoder.encode("12345"))
+                    .setEmail("admin@gmail.com")
+                    .setRoles(List.of(admin, user))
+                    .setGames(null);
+
+            userRepository.save(userEntity);
         }
     }
 }
