68ab90f0f0aa3a06f0f173bc3ea841cf90f55318
Valentin Mihaylov <vmbg00@gmail.com>
2021-03-15 01:01:43 +0200

    Minor changes to Event Listeners

--- src/main/java/bg/softuni/gamingstore/events/GameCreatedListener.java
+++ src/main/java/bg/softuni/gamingstore/events/GameCreatedListener.java
@@ -1,14 +1,19 @@
 package bg.softuni.gamingstore.events;
 
+import bg.softuni.gamingstore.services.impl.GameServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameCreatedListener {
 
+    private Logger LOGGER = LoggerFactory.getLogger(GameCreatedEvent.class);
+
     @EventListener(GameCreatedEvent.class)
     public void onStudentEvent(GameCreatedEvent event) {
-        System.out.println("Game created event received!");
-        System.out.println(event);
+        LOGGER.info("Game created event received!");
+        LOGGER.info(event.toString());
     }
 }
--- src/main/java/bg/softuni/gamingstore/events/NewUserListener.java
+++ src/main/java/bg/softuni/gamingstore/events/NewUserListener.java
@@ -1,14 +1,18 @@
 package bg.softuni.gamingstore.events;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NewUserListener {
 
+    private Logger LOGGER = LoggerFactory.getLogger(GameCreatedEvent.class);
+
     @EventListener(NewUserEvent.class)
     public void onStudentEvent(NewUserEvent event) {
-        System.out.println("New user register event received!");
-        System.out.println(event);
+        LOGGER.info("New user event received!");
+        LOGGER.info(event.toString());
     }
 }
